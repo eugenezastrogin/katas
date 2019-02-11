935218fe5e9b7bd425fa7af87b428c33d9452cf3
Eugene <aethelz@users.noreply.github.com>
2019-02-11 21:04:39 +0300

    Add snail haskell solution

---
+++ snail.hs
@@ -0,0 +1,36 @@
+-- https://www.codewars.com/kata/snail/train/haskell
+-- Given an n x n array, return the array elements arranged
+-- from outermost elements to the middle element, traveling clockwise.
+
+module Snail where
+
+main :: IO ()
+main = do
+  putStrLn $ show  $ snail [[]]
+  putStrLn $ show  $ snail [[1,2,3],[4,5,6],[7,8,9]]
+
+snail :: [[Int]] -> [Int]
+snail [[]] = []
+snail m = map (\(i, j) -> m !! i !! j) $ moveMapper 'R' (length m) (0, 0) []
+
+nextDir :: Char -> Char
+nextDir dir
+  | dir == 'R' = 'D'
+  | dir == 'D' = 'L'
+  | dir == 'L' = 'U'
+  | dir == 'U' = 'R'
+
+mv :: Char -> Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+mv dir size (i, j) visited
+  | dir == 'R' = notVisited $ map ((,) i) [j..size - 1]
+  | dir == 'D' = notVisited $ map (flip (,) j) [i..size - 1]
+  | dir == 'L' = notVisited $ map ((,) i) [j, (j - 1)..0]
+  | dir == 'U' = notVisited $ map (flip (,) j) [i, (i - 1)..0]
+  where notVisited = filter (flip notElem visited)
+
+moveMapper :: Char -> Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+moveMapper dir size start visited =
+  if length trav > 0
+  then trav ++ moveMapper (nextDir dir) size (last trav) (trav ++ visited)
+  else trav
+    where trav = mv dir size start visited
